c0cf3eb3554d5f2c47fd2103468592bd8fee432a
gurjeetsinghmundra <singhgurjeet101003@gmail.com>
2024-12-27 09:31:47 +0530

    Day 18(Fetching Data From Form)

---
+++ 8_Delete/delete_products.js
@@ -0,0 +1,17 @@
+fetch("http://localhost:8080/products")
+.then(data=>data.json())
+.then(data=>{
+    console.log(data);
+
+    document.write(`<p>number: ${data["page"]["number"]}</p>`)
+    document.write(`<p>totalPages: ${data["page"]["totalPages"]}</p>`)
+    document.write(`<p>size: ${data["page"]["size"]}</p>`)
+  
+    data["_embedded"]["products"].forEach(product=>{
+        //$ means value
+        //``
+        document.write(`<h1>${product["productName"]}</h1>`)
+        document.write(`<p>${product["_links"]["self"]["href"]}</p>`) 
+    });
+ 
+})
